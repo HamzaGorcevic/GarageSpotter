ee695fcb8219f7fb8a4bf2745e0a1c9264cc4af4
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-11-27 18:52:58 +0100

    splitting into chucks

--- Client/garagespotter/vite.config.js
+++ Client/garagespotter/vite.config.js
@@ -1,17 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        port: 5173,
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/assets/scss/_variables.scss";`,
+      },
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "./src/assets/scss/_variables.scss";`,
-            },
+  },
+  build: {
+    // Increase chunk size limit if necessary
+    chunkSizeWarningLimit: 1000, // You can adjust this value based on your needs
+
+    // Rollup options for manual chunk splitting
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Put all node_modules dependencies into a "vendor" chunk
+          }
         },
+      },
     },
+  },
 });
