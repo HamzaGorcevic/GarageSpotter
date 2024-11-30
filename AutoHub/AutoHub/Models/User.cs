8667021e0cce2a76b92f42539734fd97f29404ee
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-11-30 14:55:25 +0100

    user restrictions

--- AutoHub/AutoHub/Models/User.cs
+++ AutoHub/AutoHub/Models/User.cs
@@ -1,7 +1,7 @@
 ﻿using AutoHub.Models.Enums;
+
 namespace AutoHub.Models
 {
-
     public class User
     {
         public int Id { get; set; }
@@ -9,12 +9,27 @@ namespace AutoHub.Models
         public string Email { get; set; } = string.Empty;
         public byte[]? PasswordHash { get; set; }
         public byte[]? PasswordSalt { get; set; }
-
         public UserRole Role { get; set; }
 
-        public List<GarageSpot>? GarageSpots { get; set; }
-        public List<ElectricCharger>? ElectricChargers { get; set; }
+        public List<GarageSpot>? GarageSpots
+        {
+            get => Role == UserRole.Owner ? _garageSpots : null;
+            set { if (Role == UserRole.Owner) _garageSpots = value; }
+        }
+        private List<GarageSpot>? _garageSpots;
 
-        public List<GarageSpot>? Favorites { get;set; }
+        public List<ElectricCharger>? ElectricChargers
+        {
+            get => Role == UserRole.Owner ? _electricChargers : null;
+            set { if (Role == UserRole.Owner) _electricChargers = value; }
+        }
+        private List<ElectricCharger>? _electricChargers;
+
+        public List<GarageSpot>? Favorites
+        {
+            get => Role != UserRole.Admin ? _favorites : null;
+            set { if (Role != UserRole.Admin) _favorites = value; }
+        }
+        private List<GarageSpot>? _favorites;
     }
 }
