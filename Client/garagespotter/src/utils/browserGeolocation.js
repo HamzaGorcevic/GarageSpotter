971f19df0995134841736833acfeaeac09464a3a
HamzaGorcevic <gorcevichamza76@gmail.com>
2024-12-22 03:09:30 +0100

    has to be longer

--- Client/garagespotter/src/utils/browserGeolocation.js
+++ Client/garagespotter/src/utils/browserGeolocation.js
@@ -36,6 +36,6 @@ export const getBrowserLocation = () => {
                     "Geolocation timeout - couldn't get accurate position"
                 )
             );
-        }, 500);
+        }, 1500);
     });
 };
