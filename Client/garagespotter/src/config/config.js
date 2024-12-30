e2429c746674875e1c92c8f835d8b4c4fdf89289
CopyHamza <gorcohamza03@gmail.com>
2024-12-30 23:25:09 +0100

    backend path

--- Client/garagespotter/src/config/config.js
+++ Client/garagespotter/src/config/config.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://garagespotter.onrender.com";
-const BASE_URL = "http://garagespotter.railway.internal";
+const BASE_URL = "https://garagespotter-production-9ae2.up.railway.app";
 // const BASE_URL = "http://localhost:8080";
 // const BASE_URL = "http://localhost:5243";
 export { BASE_URL };
